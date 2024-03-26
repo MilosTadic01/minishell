4b08381c287860b52a80004e724eb1ce624f5b0b
Milos Tadic <mitadic@c4c6c4.42berlin.de>
2024-03-26 17:49:41 +0100

    Start adding authorized fts to readme; move bash script to a dir; sketch some builtins

--- README.md
+++ README.md
@@ -7,3 +7,20 @@ The Open Group Base Secifications Issue 6
 IEEE Std 1003.1, 2004 Edition
 The definition of the Shell Command Language:
 https://pubs.opengroup.org/onlinepubs/009604499/utilities/xcu_chap02.html
+
+## Authorized functions
+
+1 - user commands
+2 - system calls
+3 - library functions
+8 - sysadmin commands
+rl_lib - rl_lib
+
+* readline(3) - gnl, but official
+* rl_clear_history(rl lib) - void rl_clear_history (void); Clear the history list by deleting all of the entries, in the same manner as the History library’s clear_history() function. This differs from clear_history because it frees private data Readline saves in the history list.
+* rl_on_new_line(rl lib) - int rl_on_new_line (void); Tell the update functions that we have moved onto a new (empty) line, usually after outputting a newline.
+* rl_replace_line(rl lib) - void rl_replace_line (const char \*text, int clear_undo); Replace the contents of rl_line_buffer with text. The point and mark are preserved, if possible. If clear_undo is non-zero, the undo list associated with the current line is cleared.
+* rl_redisplay(rl lib) - void rl_redisplay(void); Change what’s displayed on the screen to reflect the current contents of rl_line_buffer. 
+* add_history(rl lib) - void add_history(line); add_history (line); If you want the user to be able to get at the line later, (with C-p for example), you must call add_history() to save the line away in a history list of such lines.
+* printf(3)
+* getent(1) - get entries from Name Service Switch libraries
--- sources/builtins/builtins.c
+++ sources/builtins/builtins.c
@@ -6,17 +6,25 @@
 /*   By: mitadic <mitadic@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 17:58:34 by mitadic           #+#    #+#             */
-/*   Updated: 2024/03/26 14:16:21 by mitadic          ###   ########.fr       */
+/*   Updated: 2024/03/26 17:43:51 by mitadic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_cat
+// This assumes that the parser will store the only possible option -n in the 2nd column
+// Else if no option, then the argument will be in the 2nd column (strarr[1])?
+// STDIN_FILENO: no. STDOUT_FILENO: yes.
+void	ft_echo(char **strarr)
 {
-	//
+	if (!ft_strcmp(strarr[1], "-n"))
+		ft_putstr_fd(strarr[2], STDOUT_FILENO);
+	else
+		ft_putstr_fd(strarr[1], STDOUT_FILENO);
+		ft_putstr_fd("\n", STDOUT_FILENO);
 }
 
 // Inaccurate depiction, what we'd pass here is the row info of the Command Table,
 // so we'd use something like 'berta->tokens[2]->path', but my C-fu isn't fresh
+// STDIN_FILENO: no. STDOUT_FILENO: no.
 void	ft_cd(t_berta *berta)
 {
 	if (!berta->tokens->path)
@@ -25,4 +33,25 @@ void	ft_cd(t_berta *berta)
 		chdir(ft_strjoin(berta->envp->PWD, berta->tokens->&path[2]));
 	else
 		chdir(berta->tokens->path);
+}
+
+
+// STDIN_FILENO: no. STDOUT_FILENO: yes.
+void	ft_pwd(char **envp)
+{
+	int	i;
+
+	i = -1;
+	while (envp[++i])
+	{
+		if (!strncmp(envp[i], "PWD=", 4))
+			ft_putstr_fd(&envp[i][4], STDOUT_FILENO);
+	}
+}
+
+// if u 'export PWD=<newpath>' shit goes a little haywire in bash
+// Authorized: getenv. Unauthorized: putenv, setenv, unsetenv.
+// STDIN_FILENO: no. STDOUT_FILENO: no.
+void	ft_export(void)
+{
 }
